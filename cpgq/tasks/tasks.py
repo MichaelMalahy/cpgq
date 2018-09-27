dab81a483a6f7c4e4c44b03f2eb55557f0c2ed47
Michael Malahy <mpmalahy@ou.edu>
2018-09-27 13:43:55 -0500

    added dice_sides and multi_dice tasks with signature chain

--- cpgq/tasks/tasks.py
+++ cpgq/tasks/tasks.py
@@ -1,20 +1,38 @@
 from celery.task import task
 from celery import signature, group
 
-@task
-def add(x,y):
-    return x + y
+#@task
+#def add(x,y):
+#    return x + y
+
+#@task
+#def double(x):
+#    return x * 2
+
+#@task
+#def triple(x):
+#    return x * 3
 
 @task
-def double(x):
-    return x * 2
+#def test_grouping(x,y):
+#    chain = (add.s(x,y) | group(double.s(), triple.s()))
+#    chain.delay()
+#    return "Kicked Off tasks"
 
 @task
-def triple(x):
-    return x * 3
+def dice_sides(sides):
+   return random.randrange(sides) + 1
 
 @task
-def test_grouping(x,y):
-    chain = (add.s(x,y) | group(double.s(), triple.s()))
+def multi_dice(number, sides):
+    dice = range(number)
+    result = []
+    for i in dice:
+        result.append(dice_sides(sides))
+    return result
+
+@task
+def dice_roll_chain(number, sides):
+    chain = (dice_sides.s(sides) | multi_dice.s(number,sides)
     chain.delay()
-    return "Kicked Off tasks"
+    return "Kicked off tasks"
